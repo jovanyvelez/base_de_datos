b86fecfec1d89b2f0a1284c8f88adab578709dea
jovany velez <jovany.velez@udea.edu.co>
2023-12-08 11:23:41 -0500

    back button

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle:Handle = async ({ event, resolve }) => {
-    
-    console.time('handle');
-    const response = await resolve(event);
-    console.timeEnd('handle');
-    return response;
-}
