bdc056e2ccf0e4118215724f2ab6e09c62cbf204
jovany velez <jovany.velez@udea.edu.co>
2023-12-12 13:15:17 -0500

    flex

--- src/routes/categories/raiz/+page.svelte
+++ src/routes/categories/raiz/+page.svelte
@@ -1,32 +1,45 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	
+
 	export let data: PageData;
-
 </script>
-
 
 <h1>Bienvenido</h1>
 <a href="/">Back</a>
 <p>
-	Categoria:
 	{#await data.test}
 		Loading...
 	{:then value}
-		{value[0].categoria}
-		{#each value as val (val.product_id)}
-		<ul>
-			<li>{val.name}</li>
-			<img src={val.secure_url} alt="">
-		</ul>	
-
-			
-		{/each}
-
+		<div class="bg">
+			{#each value as val (val.product_id)}
+				<div>
+					<ul>
+						<li>{val.name}</li>
+						<img src={val.secure_url} alt="" />
+					</ul>
+					<h6>{val.categoria}</h6>
+				</div>
+			{/each}
+		</div>
 	{:catch error}
 		{error.message}
 	{/await}
 </p>
 
-
 <a href="/">Back</a>
+
+<style>
+	
+	h6 {
+		text-align: center;
+	}
+	.bg {
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		align-items: center;
+		height: 100vh;
+		margin-right: 5px;
+		padding: 5px;
+	}
+</style>
